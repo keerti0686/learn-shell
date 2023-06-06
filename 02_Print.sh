7548450d1c9e5bb48d8642abdd21a10ed0f907b5
keert1 <kshanumanthrao09@gmail.com>
2023-06-06 02:13:23 -0400

    List of shell scripting topics

--- 02_Print.sh
+++ 02_Print.sh
@@ -15,3 +15,9 @@ echo -e "\e[33mHello in Yellow Color\e[0m"
 echo -e "\e[34mHello in Blue Color\e[0m"
 echo -e "\e[35mHello in Magenta Color\e[0m"
 echo -e "\e[36mHello in Cyan Color\e[0m"
+
+echo Diksha
+
+echo -e "\e[31mDiksha in Red color\e]0m"
+echo -e "\e[32mDiksha in green color\e]0m"
+echo -e "\e[33mDiksha in yellow color\e]0m"
